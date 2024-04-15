b910e88b616cd6fa4b087162f2b63300b67413cf
pythonfighter <166857372+pythonfighter@users.noreply.github.com>
2024-04-15 18:24:36 +0530

    Add files via upload

---
+++ malid.py
@@ -0,0 +1,12 @@
+
+adj = input("Adjective 1: ")
+noun = input("noun: ")
+colour = input("Colour: ")
+feature = input("rose's Feature: ")
+adj3 = input("Adjective 2: ")
+object = input("Object in the sky: ")
+
+malib = f"In the {adj} garden, a single rose bloomed under the {noun} sky. Its petals shimmered like {colour} in the moonlit light. Nearby, a {adj3} cat prowled,\
+eyes fixed on the mysterious flower. As the night deepened, the rose\'s {feature} drew the cat closer, weaving a tale of enchantment beneath the {oject}."
+
+print(malib)
